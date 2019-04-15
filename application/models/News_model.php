372ca49178ed7b1ba1c98f167ee5fc1e4fa03ee4
Thom Harrington <thomas.harrington@seattlecentral.edu>
2019-04-14 22:08:39 -0700

    A4 changes and add .htaccess

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -7,6 +7,7 @@ class News_model extends CI_Model {
             $this->load->database();
     }
 
+    
     public function get_news($slug = FALSE)
     {
             if ($slug === FALSE)
@@ -17,5 +18,20 @@ class News_model extends CI_Model {
 
             $query = $this->db->get_where('sp19_news', array('slug' => $slug));
             return $query->row_array();
+    }
+    
+    public function set_news()
+    {
+        $this->load->helper('url');
+
+        $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'text' => $this->input->post('text')
+        );
+
+        return $this->db->insert('sp19_news', $data);
     }
 }
